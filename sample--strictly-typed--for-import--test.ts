3f839fbbb8cb173f66a38dbeed32c5c91e51f7d5
chuntaro <chuntaro@sakura-games.jp>
2015-12-23 18:41:40 +0900

    Add some test code

--- sample--strictly-typed--for-import--test.ts
+++ sample--strictly-typed--for-import--test.ts
@@ -15,7 +15,7 @@
 import {mat3, mat4, vec3, vec4} from './gl-matrix';
 
 let v3 = vec3.fromValues(0, 1, 2);
-let v4 = vec4.create();
+let v4 = vec4.fromValues(0, 1, 2, 3);
 let m3 = mat3.create();
 let m4 = mat4.create();
 
@@ -28,3 +28,12 @@ console.log(m4);
 // console.log(vec3.length(v3)); // compile error, length is Float32Array's attribute
 console.log(vec3.len(v3)); // ok -> 2.23606797749979
 console.log(v3.length); // ok -> 3
+
+// console.log(vec3.len(v4)); // compile error
+console.log(vec3.len(<vec3>v4)); // ok -> 2.23606797749979
+
+function uniform3fv(v: Float32Array): void {
+  console.log(v);
+}
+
+uniform3fv(m3); // ok!
